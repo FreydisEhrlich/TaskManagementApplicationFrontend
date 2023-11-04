dcfc0abc6209d33f3b2aaac439d4260b11d61d44
freyd <freydis.ehrlich@gmail.com>
2023-11-04 10:12:50 +0200

    Initial commit!

---
+++ src/app/task.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environments';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Task } from './task';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+//API server base URL
+private apiServerUrl = environment.apiBaseUrl;
+
+//Inject HTTPClient in the service layer constructor
+constructor(private http: HttpClient) { }
+
+public getTasks(): Observable<Task[]>{
+  return this.http.get<Task[]>(`${this.apiServerUrl}/task/all`)
+}
+
+public addTask(task: Task): Observable<Task>{
+  return this.http.post<Task>(`${this.apiServerUrl}/task/add`, task)
+}
+
+  public updateTask(task: Task): Observable<Task>{
+    return this.http.put<Task>(`${this.apiServerUrl}/task/update`, task)
+  }
+
+   public deleteTask(taskId: number):Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/task/delete/${taskId}`)
+  }
+
+}
---
+++ src/app/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environments';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+//API server base URL
+private apiServerUrl = environment.apiBaseUrl;
+
+//Inject HTTPClient in the service layer constructor
+constructor(private http: HttpClient) { }
+
+public getUsers(): Observable<User[]>{
+  return this.http.get<User[]>(`${this.apiServerUrl}/users/all`)
+}
+
+public addUser(user: User): Observable<User>{
+  return this.http.post<User>(`${this.apiServerUrl}/users/add`, user)
+}
+
+  public updateUser(user: User): Observable<User>{
+    return this.http.put<User>(`${this.apiServerUrl}/users/update`, user)
+  }
+
+   public deleteUser(userId: number):Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/users/delete/${userId}`)
+  }
+
+}
